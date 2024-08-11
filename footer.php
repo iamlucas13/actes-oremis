0b5a5dd43e0f0a3d76d7e6a0233fd0c5d496a613
Lucas VOLET <lucas.volet@oremis.fr>
2024-08-11 11:01:03 +0200

    debug mod

--- footer.php
+++ footer.php
@@ -7,7 +7,7 @@
                 <ul class="list-unstyled">
                 <li>© <?php echo date("Y"); ?> OREMIS. Tous droits réservés.</li>
                 <li><a href="changelog">changelog</a></li>
-                <li><a href="index">**DEBUG retour à la page principale**</a></li>
+                <li><a href="../index">**DEBUG retour à la page principale**</a></li>
             </div>
             <div class="col-md-6 text-md-right">
                 <h5>Contact</h5>
